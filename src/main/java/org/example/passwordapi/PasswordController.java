c4902a11ac10781250febb3760322a52628833d4
frankEatsNoodles <154846711+frankEatsNoodles@users.noreply.github.com>
2026-01-17 20:55:17 -0500

    Update PasswordController.java

--- src/main/java/org/example/passwordapi/PasswordController.java
+++ src/main/java/org/example/passwordapi/PasswordController.java
@@ -46,45 +46,5 @@ public class PasswordController {
             dbManager.savePassword(username, password);
 
             return "Password received and encrypted!";
-    @PostMapping
-    public String savePassword(@RequestBody Map<String, Object> passwordData) {
-        String username = (String) passwordData.get("username");
-        String password = (String) passwordData.get("password");
-        String service = (String) passwordData.get("service");
-        if (password != null && username != null && service != null) {
-            String encryptedPassword = EncryptionUtil.caesarEncrypt(password, SHIFT);
-            System.out.println("Encrypted password: " + encryptedPassword);
-
-            // Prepare JSON for Pi receiver
-            String json = "{"
-                    + "\"username\":\"" + username + "\","
-                    + "\"service\":\"" + service + "\","
-                    + "\"ciphertext\":\"" + encryptedPassword + "\","
-                    + "\"iv\":\"dummyiv\","
-                    + "\"salt\":\"dummysalt\""
-                    + "}";
-
-            // Send POST to Pi receiver
-            try {
-                URL url = new URL("http://localhost:5000/store");
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Content-Type", "application/json");
-                conn.setDoOutput(true);
-
-                try (OutputStream os = conn.getOutputStream()) {
-                    os.write(json.getBytes());
-                }
-
-                int responseCode = conn.getResponseCode();
-                System.out.println("Pi receiver response code: " + responseCode);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return "Error forwarding to Pi receiver!";
-            }
-
-            return "Password received, encrypted, and forwarded!";
-        }
-        return "No password provided or service missing!";
-    }
+    
 }
